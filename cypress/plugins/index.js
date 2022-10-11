49c03ca754b33262d46fdaadb4932603ec80d6c4
Preston Williamson <preston.williamson@alumni.uncg.edu>
2022-10-11 19:15:17 -0400

    Committing to remove videos of passing results

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,21 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const { defineConfig } = require('cypress')
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+// need to install the "del" module as a dependency
+// npm i del --save-dev
+const del = require('del')
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+  e2e: {
+    setupNodeEvents(on, config) {
+      on('after:spec', (spec, results) => {
+        if (results && results.stats.failures === 0 && results.video) {
+          // `del()` returns a promise, so it's important to return it to ensure
+          // deleting the video is finished before moving on
+          return del(results.video)
+        }
+      })
+    }
+  }
+})
